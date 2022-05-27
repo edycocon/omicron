0408b48f550d6dcdaf4a94ac17cbad4c9cd135ea
Javier Ferrer <JavierFerrer@galileo.edu>
2022-05-27 17:28:35 -0600

    Pasamos los primeros test :'D

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -13,6 +13,8 @@
 
 static void syscall_handler (struct intr_frame *);
 
+struct lock filesys_lock;
+
 struct stArchivo {
   int fd;
   struct file* archivo;
@@ -23,6 +25,7 @@ void
 syscall_init (void) 
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+  lock_init (&filesys_lock);
 }
 
 static void
@@ -31,17 +34,19 @@ syscall_handler (struct intr_frame *f UNUSED)
   int sys_code = *(int*)f->esp;
   char* nombre_archivo;
 
-  if (!validar_puntero(f->esp))
-  {
-    exit(-1);
-  }
+  validar_puntero(f->esp);
 
   switch (sys_code) {
     case SYS_HALT:
       shutdown_power_off();
+      PANIC ("executed an unreachable statement");
       break;
     case SYS_EXIT:
-      exit(sys_code);
+      validar_puntero((int*)f->esp + 1);
+  
+      int status = *((int*)f->esp + 1);
+
+      exit(status);
     break;
       case SYS_EXEC:
       /* code */
@@ -57,7 +62,7 @@ syscall_handler (struct intr_frame *f UNUSED)
       break;
     case SYS_OPEN:
       nombre_archivo = (char*)(*((int*)f->esp + 1));
-      f->eax = open(nombre_archivo);
+      f->eax = (uint32_t)open(nombre_archivo);
       break;
     case SYS_FILESIZE:
       /* code */
@@ -66,7 +71,7 @@ syscall_handler (struct intr_frame *f UNUSED)
       /* code */
       break;
     case SYS_WRITE:
-      f->eax = write(f);
+      f->eax =  (uint32_t)sys_write(f);
       break;
     case SYS_SEEK:
       /* code */
@@ -82,7 +87,7 @@ syscall_handler (struct intr_frame *f UNUSED)
       break;
   }
 
-  thread_exit ();
+  //thread_exit ();
 }
 
 void validar_puntero(void *puntero) {
@@ -98,9 +103,6 @@ void validar_puntero(void *puntero) {
 }
 
 void exit(int status) {
-  if (status != -1) {
-    validar_puntero(status);
-  }
 
   struct thread *tActual = thread_current();
 
@@ -111,9 +113,7 @@ void exit(int status) {
 
 int open(const char* file) {
 
-  if (!validar_puntero(file)) {
-    exit(-1);
-  }
+  validar_puntero(file);
 
   struct file* archivo_act;
   struct stArchivo* archivo_tmp = palloc_get_page(0);
@@ -122,9 +122,12 @@ int open(const char* file) {
     return -1;
   }
 
+  lock_acquire (&filesys_lock);
+
   archivo_act = file_open(file);
 
   if (archivo_act == NULL) {
+    lock_release (&filesys_lock);
     return -1;
   }
 
@@ -133,10 +136,12 @@ int open(const char* file) {
 
   list_push_back(&(thread_current()->archivos), &(archivo_tmp->elem));
 
-  return -1;
+  lock_release (&filesys_lock);
+
+  return archivo_tmp->fd;
 }
 
-int write (struct intr_frame *f UNUSED) {
+int sys_write (struct intr_frame *f UNUSED) {
   validar_puntero((int*)f->esp + 1);
   validar_puntero((int*)f->esp + 2);
   validar_puntero((int*)f->esp + 3);
@@ -144,27 +149,33 @@ int write (struct intr_frame *f UNUSED) {
   int fd = *((int*)f->esp + 1);  
   char* buffer = (char*)(*((int*)f->esp + 2)); 
   unsigned size = (*((int*)f->esp + 3));
+  int written_bytes = 0;
 
-  //Que hago cuando venga STDIN_FILENO
-  if (fd == STDOUT_FILENO){
+  lock_acquire(&filesys_lock);
+
+  if (fd == 1){
     putbuf(buffer, size);
+    lock_release (&filesys_lock);
     return size;
 
   } else {
-    struct stArchivo archivo = obtener_Archivo(fd);
-    int written_bytes = 0;
+    struct stArchivo *archivo = obtener_Archivo(fd);
 
     if (archivo == NULL) {
+      lock_release (&filesys_lock);
       return 0;
     }
-
-    written_bytes = file_write(archivo->archivo, buffer, size);
-
-    return written_bytes;
+  
+    written_bytes = (int)file_write(archivo->archivo, buffer, size);
+    
   }
+
+  lock_release (&filesys_lock);
+
+  return written_bytes;
 }
 
-struct stArchivo obtener_Archivo(int fd, enum fd_search_filter flag) {
+struct stArchivo* obtener_Archivo(int fd) {
   struct thread *t = thread_current();
   struct list_elem *e;
 
